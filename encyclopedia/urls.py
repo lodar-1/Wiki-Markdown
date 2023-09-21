b687c56bce1db72a675f4bc68b8e226c20d1169b
Daryl <lodard@gmail.com>
2023-09-21 22:15:27 +0200

    Added edit functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     path("add/", views.add, name="add"),
+    path("edit/", views.edit, name="edit"), 
+    path("entry/", views.entry, name="entry"),   
     path("<str:entry>", views.entry, name="entry")
     
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,7 +3,7 @@ import markdown2
 from django import forms
 from django.http import HttpResponseRedirect
 from . import util
-
+from django.urls import reverse
 
 class newSearch(forms.Form):
 	searchphrase = forms.CharField()
@@ -18,6 +18,24 @@ def index(request):
     })
 
 #DM 20230919
+def edit(request):
+	print("REQUEST FROM EDIT: ")
+	print(request)
+	if request.method == "POST":
+		form = addForm(request.POST)
+		if form.is_valid():
+			stitle = form.cleaned_data["title"]			
+			util.save_entry(stitle, form.cleaned_data["content"])
+			strEntry = markdown2.markdown(form.cleaned_data["content"])
+			#return render(request, "encyclopedia/entry.html", {"title": entry, "entry":strEntry})
+			#return HttpResponseRedirect(reverse("encyclopedia/entry.html"))#, {"title": form.cleaned_data["title"], "entry":strEntry}))
+			return HttpResponseRedirect("/wiki/" + stitle)
+		else:
+			return render(request, "encyclopedia/editentry.html", {"error": "A validation error occcured."})	
+	else:
+		return render(request, "encyclopedia/editentry.html")
+		
+	
 def add(request):
 	if request.method == "POST":
 		listitems = util.list_entries()
@@ -25,25 +43,34 @@ def add(request):
 			if li.upper() == request.POST.get("title").strip().upper():
 				return render(request, "encyclopedia/addentry.html", {"error": 'An entry with this title already exists', "stitle": request.POST.get("title").strip(), "scontent": request.POST.get("content") })
 		form = addForm(request.POST)
-		print(form) 
 		if form.is_valid():
 			util.save_entry(form.cleaned_data["title"], form.cleaned_data["content"])
 			return render(request, "encyclopedia/entry.html", {"title": form.cleaned_data["title"], "entry":form.cleaned_data["content"]})
+		#	return render(request, "form.cleaned_data['title'")
 		else:
 			return render(request, "encyclopedia/addentry.html", {"error": "An error occcured."})	
 	else:
 		return render(request, "encyclopedia/addentry.html")
 
 def entry(request, entry):
-	#if entry=="search":
-	#	return search(request)
-	strEntry = util.get_entry(entry)
-	if strEntry is not None:
-		strEntry = markdown2.markdown(strEntry)
-	else:
-		strEntry = "Error: Page not foundxx"
-		entry = "Error - Page not found"	
-	return render(request, "encyclopedia/entry.html", {"title": entry, "entry":strEntry})
+	if request.method == "POST":
+		strEntry = util.get_entry(entry)
+		
+		if strEntry == None:
+			strEntry = f"Error: Page not found ({search})"
+			entry = "Error - Page not found"	
+		print("REQUEST FROM ENTRY")
+		print(request)
+		return render(request, "encyclopedia/editentry.html", {"title": entry, "content":strEntry})					
+
+	else:	
+		strEntry = util.get_entry(entry)
+		if strEntry is not None:
+			strEntry = markdown2.markdown(strEntry)
+		else:
+			strEntry = "Error: Page not foundxx"
+			entry = "Error - Page not found"	
+		return render(request, "encyclopedia/entry.html", {"title": entry, "entry":strEntry})
 
 def search(request):
 	if request.method == "POST":
