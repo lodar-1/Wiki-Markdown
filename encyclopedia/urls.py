698e6d4adc57851d66624611bbb0a6c3b4f663d3
Daryl <lodard@gmail.com>
2023-09-22 11:52:01 +0200

    added random entry selection

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("add/", views.add, name="add"),
     path("edit/", views.edit, name="edit"), 
-    path("entry/", views.entry, name="entry"),   
+    path("entry/", views.entry, name="entry"), 
+    path("randomentry/", views.randomentry, name="randomentry"),  
     path("<str:entry>", views.entry, name="entry")
     
 ]
